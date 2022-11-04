34180106c8c8bd0f7898405121c8493fde7c59ef
HV Nữa <56875666+HVNuaIT@users.noreply.github.com>
2022-11-04 13:54:18 +0700

    Cập nhật sản phẩm Mới nhất

--- ShopBanHang/Controllers/HomeController.cs
+++ ShopBanHang/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ShopBanHang.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,8 +10,15 @@ namespace ShopBanHang.Controllers
     public class HomeController : Controller
     {
         // GET: Home
+        Shop db = new Shop();
         public ActionResult Index()
         {
+            return View();
+        }
+        public ActionResult SanPhamMoiNhat()
+        {
+           
+
             return View();
         }
     }
